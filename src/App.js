3347a2d0cfe833d0c50279149fed911f78c97ef7
Hugo COLLIN <hugo.collin@inist.fr>
2024-04-16 14:41:19 +0200

    state + render

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
+
 function App() {
    // state
+   const [count, setCount] = useState(41);
 
    // behaviours
 
    // render
+   return <h1>{count}</h1>
 }
 
 export default App;
