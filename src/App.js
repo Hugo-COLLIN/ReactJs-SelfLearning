d1ab6371f7ed6c2c76f9a17a0fca53ddc0a79ebd
Hugo COLLIN <hugo.collin@inist.fr>
2024-04-16 14:55:50 +0200

    behaviour : increment button

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ function App() {
    const [count, setCount] = useState(1);
 
    // behaviours
+   const handleClick = () => {
+    console.log(count);
+    setCount(count + 1);
+    console.log(count);
+   }
 
    // render
    return (
     <>
       <h1>{count}</h1>
-      <button>Increment +</button>
+      <button onClick={handleClick}>Increment +</button>
+      <button onClick={() => alert("2222")}>test</button>
     </>
    );
 }
