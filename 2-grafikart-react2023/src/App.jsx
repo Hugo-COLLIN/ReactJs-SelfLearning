52c8a5693b8146624ed3ad66663d1263a745d209
Hugo COLLIN <hugo.collin@inist.fr>
2024-04-19 16:20:56 +0200

    4.12:13 jsx

--- 2-grafikart-react2023/src/App.jsx
+++ 2-grafikart-react2023/src/App.jsx
@@ -1,5 +1,27 @@
+const title = "Hello <strong>world</strong>";
+const style = { color: 'red', backgroundColor: 'blue' };
+
+
 function App() {
-  return 'Hello world!'
+  // --- STATE ---
+
+  // --- BEHAVIOUR ---
+  const handleClick = (e) => {
+    console.log(e);
+    e.preventDefault();
+    e.stopPropagation();
+    alert("Title clicked!")
+  }
+
+  // --- RENDER ---
+  return <>
+          <h1 className={title} style={style} onClick={handleClick}>{title}</h1>
+          <input type="text"/>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem, eaque. 
+            Repudiandae voluptatibus optio alias impedit quaerat velit praesentium nemo repellat, et illo facere natus libero nihil vero tempore quo non.
+          </p>
+         </>
 }
 
-export default App
+export default App;
