fe9ff296abdaec258f790cb6baa3c7b1146faa81
Hugo COLLIN <hugo.collin@inist.fr>
2024-04-22 15:02:24 +0200

    09 useEffect timer

--- 2-grafikart-react2023/09-useEffect-exoCounter/src/App.jsx
+++ 2-grafikart-react2023/09-useEffect-exoCounter/src/App.jsx
@@ -2,26 +2,48 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import { Input } from "./components/forms/Input";
-import { Checkbox } from "./components/forms/Checkbox"
 
 function App() {
   // --- STATE ---
   const [durationSeconds, setDurationSeconds] = useState(5);
+  const [secondsLeft, setSecondsLeft] = useState(durationSeconds);
   
   // --- BEHAVIOURS ---
+  const handleChange = (v) => {
+    setDurationSeconds(v);
+    setSecondsLeft(v)
+  }
+
   useEffect(() => {
-    setTimeout(() => {
-      console.log(`Timer de ${durationSeconds} secondes écoulé`);
-    }, durationSeconds * 1000);
+    // setTimeout(() => {
+    //   console.log(`Timer de ${durationSeconds} secondes écoulé`);
+    // }, durationSeconds * 1000);
+
+    // setSecondsLeft(duration) // Mauvaise idée, le composant est du coup rerendu 2 fois de suite - NE PAS METTRE DE SETTER A LA RACINE D'UN 
+    const timer = setInterval(() => {
+      // setSecondsLeft((s) => s - 1);
+      setSecondsLeft((s) => {
+        if (s <= 1) {
+          clearInterval(timer);
+          return 0;
+        }
+        return s - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
   }, [durationSeconds])
+
 
   // --- RENDER ---
   return <div className="container my-3 stack">
     <Input
       placeholder="Timer"
       value={durationSeconds}
-      onChange={setDurationSeconds}
+      // onChange={setDurationSeconds}
+      onChange={handleChange}
     />
+    Décompte: {secondsLeft}
   </div>
 }
 
