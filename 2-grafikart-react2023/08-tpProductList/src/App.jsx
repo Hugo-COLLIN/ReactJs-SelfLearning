068aa2fa97634749f702e6807f3c8ff0b6633048
Hugo COLLIN <hugo.collin@inist.fr>
2024-04-22 11:13:15 +0200

    start tp products

---
+++ 2-grafikart-react2023/08-tpProductList/src/App.jsx
@@ -0,0 +1,21 @@
+const PRODUCTS = [
+  { category: 'Fruits', price: "$1", stocked: true, name: "Apple" },
+  { category: 'Vegetables', price: "$2", stocked: false, name: "Carrot" },
+  { category: 'Fruits', price: "$0.5", stocked: true, name: "Banana" },
+  { category: 'Vegetables', price: "$1.5", stocked: true, name: "Tomato" },
+  { category: 'Fruits', price: "$2.5", stocked: false, name: "Orange" },
+  { category: 'Vegetables', price: "$3", stocked: true, name: "Broccoli" }
+];
+
+function App() {
+  // --- STATE ---
+
+  // --- BEHAVIOUR ---
+  
+  // --- RENDER ---
+  return <>
+  
+  </>
+}
+
+export default App;
