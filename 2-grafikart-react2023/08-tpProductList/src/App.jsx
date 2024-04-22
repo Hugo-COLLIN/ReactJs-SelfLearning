ea81999a4f22039064c0a7990da891a31a3d7ee6
Hugo COLLIN <hugo.collin@inist.fr>
2024-04-22 11:49:42 +0200

    08.15:40

--- 2-grafikart-react2023/08-tpProductList/src/App.jsx
+++ 2-grafikart-react2023/08-tpProductList/src/App.jsx
@@ -1,21 +1,61 @@
+/* eslint-disable react/prop-types */
+import { Checkbox } from "../../07-dataStream/src/components/forms/Checkbox";
+import { Input } from "../../07-dataStream/src/components/forms/Input";
+import { ProductCategoryRow } from "../../07-dataStream/src/components/products/ProductCategoryRow";
+import { ProductRow } from "../../07-dataStream/src/components/products/ProductRow";
+
 const PRODUCTS = [
   { category: 'Fruits', price: "$1", stocked: true, name: "Apple" },
-  { category: 'Vegetables', price: "$2", stocked: false, name: "Carrot" },
   { category: 'Fruits', price: "$0.5", stocked: true, name: "Banana" },
-  { category: 'Vegetables', price: "$1.5", stocked: true, name: "Tomato" },
   { category: 'Fruits', price: "$2.5", stocked: false, name: "Orange" },
+  { category: 'Vegetables', price: "$2", stocked: false, name: "Carrot" },
+  { category: 'Vegetables', price: "$1.5", stocked: true, name: "Tomato" },
   { category: 'Vegetables', price: "$3", stocked: true, name: "Broccoli" }
 ];
 
 function App() {
-  // --- STATE ---
-
-  // --- BEHAVIOUR ---
-  
-  // --- RENDER ---
-  return <>
-  
-  </>
+  return <div className="container my-3">
+    <SearchBar/>
+    <ProductTable products={PRODUCTS}/>
+  </div>
 }
+
+function SearchBar () {
+  return <div>
+    <div className="mb-3">
+      <Input value="" onChange={() => null} placeholder="Rechercher..."/>
+      <Checkbox id="stocked" checked={false} onChange={() => null} label="N'afficher que les procuits en stock"/>
+    </div>
+  </div>
+}
+
+function ProductTable ({products}) {
+  // --- STATE ---
+  const rows = [];
+  let lastCategory = null;
+
+  // --- BEHAVIOURS ---
+  for (let product of products) {
+    if (product.category !== lastCategory) 
+      rows.push(<ProductCategoryRow key={product.category} name={product.category}/>);
+
+    lastCategory = product.category;
+    rows.push(<ProductRow product={product} key={product.name}/>)
+  }
+
+  // --- RENDER ---
+  return <table className="table">
+    <thead>
+      <tr>
+        <th>Nom</th>
+        <th>Prix</th>
+      </tr>
+    </thead>
+    <tbody>
+      {rows}
+    </tbody>
+  </table>
+}
+
 
 export default App;
